9c04c97c7beec48756dc6deb97508cc419664a3e
sharifboy <sharifboy@gmil.com>
2023-07-14 10:29:35 +0500

    added another code

--- app.js
+++ app.js
@@ -17,3 +17,22 @@ function hightlight(strings, ...values) {
 const sentence = hightlight`Hi my name is${str}, my age is${age} years old`;
 console.log(hightlight`Hi my name is${str}, my age is${age} years old`);
 document.body.innerHTML = sentence;
+
+
+// FUNCTION CONSTRUCTOR
+
+function User(name, age, country) {
+    if (!new.target) {
+        return new User(name, age, country);
+    }
+    this.name = name;
+    this.age = age;
+    this.country = country;
+    return this;
+}
+
+const person1 = User("John", 30, "USA");
+const person2 = User("STEVE", 40, "Brazil");
+
+console.log(person1, "person1");
+console.log(person2, "person2");
